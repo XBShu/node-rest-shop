3d8ef4c3ed62cce79825d9b3614cae5984885914
Shuxin Xu Bao <sxb204@nyu.edu>
2020-10-19 13:27:42 +0200

    preventing CORS errors

--- app.js
+++ app.js
@@ -10,6 +10,19 @@ app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
+app.use((req,res,next) => {
+    res.header("Access-Control-Allow-Origin", '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+
+    if(req.method === 'OPTIONS') { //browser will always send an OPTIONS request first
+        res.header('Acess-Control-Allow-Methods','PUT, POST, PATCH, DELETE, GET');
+        return res.status(200).json({});
+    }
+
+});
+
+
+//Routes handling requests
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
 
