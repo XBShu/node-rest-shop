f81a5de761d06c06a5414f319348f17a0d1d69f5
Shuxin Xu Bao <sxb204@nyu.edu>
2020-10-20 13:56:03 +0200

    comments

--- api/models/order.js
+++ api/models/order.js
@@ -1,8 +1,10 @@
 const mongoose = require('mongoose');
 
 const orderSchema = mongoose.Schema({
-    _id: mongoose.Schema.Types.ObjectId,
+    //ID for order
+    _id: mongoose.Schema.Types.ObjectId, 
     //this schema is conntected to the "Product" schema
+    //ID of product ordered
     product: {type: mongoose.Schema.Types.ObjectId, ref: 'Product', required: true},
     quantity: {type: Number, default: 1},
 });
