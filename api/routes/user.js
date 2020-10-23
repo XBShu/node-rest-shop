ecebd1a651f3264f57f8a884c9df1860ebec08b0
Shuxin Xu Bao <sxb204@nyu.edu>
2020-10-23 16:27:34 +0200

    added tokens

--- api/routes/user.js
+++ api/routes/user.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 const User = require('../models/user');
 
 router.post('/signup', (req,res,next) => {
@@ -47,7 +48,19 @@ router.post('/login', (req,res,next) => {
                 }) 
             } 
             if(result) {
-                return res.status(200).json({message: 'Auth successful'});
+                //web token
+                const token = jwt.sign({
+                    email: user[0].email,
+                    userId: user[0]._id,
+                }, 
+                process.env.JWT_KEY,
+                {
+                    expiresIn: "1h"
+                });
+                return res.status(200).json({
+                    message: 'Auth successful',
+                    token: token,
+                });
             }
             res.status(401).json({ //using 404 opens it for brute force attacks
                 message: "Auth failed",
